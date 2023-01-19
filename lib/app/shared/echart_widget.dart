a5fa024d9fcf4278295ea907b8efed01d0fc81d8
mahidul-islam <mizihan84@gmail.com>
2023-01-19 22:52:07 +0600

    added echarts lib should be working

---
+++ lib/app/shared/echart_widget.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_echarts/flutter_echarts.dart';
+
+class EChartWidget extends StatelessWidget {
+  const EChartWidget({
+    super.key,
+    this.optionScript = '',
+    this.extraScript = '',
+    this.interactive = true,
+  });
+
+  final String optionScript;
+  final String extraScript;
+  final bool interactive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AbsorbPointer(
+      absorbing: !interactive,
+      child: Echarts(
+        captureAllGestures: false,
+        captureVerticalGestures: false,
+        captureHorizontalGestures: false,
+        option: utf8.decode(base64.decode(optionScript)),
+        extraScript: utf8.decode(base64.decode(extraScript)),
+      ),
+    );
+  }
+}
